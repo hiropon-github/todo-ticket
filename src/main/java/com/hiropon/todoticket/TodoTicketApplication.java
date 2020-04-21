5f649ee9feba9d343e5f5c26ac6fb9765a785767
Hiropon <36543853+hiropon-github@users.noreply.github.com>
2020-04-21 16:40:39 +0900

    初回コミット

---
+++ src/main/java/com/hiropon/todoticket/TodoTicketApplication.java
@@ -0,0 +1,13 @@
+package com.hiropon.todoticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTicketApplication.class, args);
+	}
+
+}
